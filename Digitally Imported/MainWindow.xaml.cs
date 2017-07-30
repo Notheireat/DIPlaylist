1ac51da86de087a0e625f1fa2a574879837304f0
Joseph <crackbite@protonmail.com>
2017-07-30 23:18:07 +0500

    Start

---
+++ Digitally Imported/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Digitally_Imported
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Настройка шрифта для статусного lable'а и установка активности кнопки
+        /// </summary>
+        /// <param name="status">Статус операции</param>
+        private void Status(bool status)
+        {
+            if (status)
+            {
+                statusLabel.Foreground = Brushes.Green;
+                statusLabel.FontWeight = FontWeights.Bold;
+            }
+            else
+            {
+                statusLabel.Foreground = Brushes.Red;
+            }
+            btnStart.IsEnabled = true;
+        }
+
+        private async void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            btnStart.IsEnabled = false;
+            progressBar.Value = 0;
+            statusLabel.FontWeight = FontWeights.Normal;
+            statusLabel.Foreground = Brushes.Black;
+            statusLabel.Content = "Создание временного почтового адреса...";
+
+            progressBar.Value++;
+            // Получение временного почтового адреса
+            statusLabel.Content = await TempMail.GetTempMail();
+            if (String.IsNullOrEmpty(Settings.TmAddress))
+            {
+                Status(false);
+                return;
+            }
+
+            progressBar.Value++;
+            // Получение токена для регистрации
+            statusLabel.Content = await DI.GetToken();
+            if (String.IsNullOrEmpty(Settings.DiToken))
+            {
+                Status(false);
+                return;
+            }
+
+            progressBar.Value++;
+            // Регистрация нового аккаунта
+            statusLabel.Content = await DI.GoReg();
+            if (statusLabel.Content.ToString().ToLower().Contains("di"))
+            {
+                Status(false);
+                return;
+            }
+
+            progressBar.Value++;
+            // Получение письма с премиум-ссылкой
+            statusLabel.Content = await TempMail.GetLetter();
+            if (String.IsNullOrEmpty(Settings.TmLetterID))
+            {
+                Status(false);
+                return;
+            }
+
+            progressBar.Value++;
+            // Получение премиум-ссылки из письма
+            statusLabel.Content = await TempMail.GetPremiumURL();
+            if (String.IsNullOrEmpty(Settings.DiPremiumURL))
+            {
+                Status(false);
+                return;
+            }
+
+            progressBar.Value++;
+            // Активация премиума и получение JSON-строки с информацией для плейлиста
+            statusLabel.Content = await DI.GoPremium();
+            if (String.IsNullOrEmpty(Settings.DiPlaylistJS))
+            {
+                Status(false);
+                return;
+            }
+
+            progressBar.Value++;
+            // Создание и сохранение плейлиста
+            statusLabel.Content = await Playlist.GoPlaylist();
+            if (statusLabel.Content.ToString().ToLower().Contains("ошибка"))
+            {
+                Status(false);
+                return;
+            }
+            Status(true);
+        }
+    }
+}
---
+++ Digitally Imported/Settings.cs
@@ -0,0 +1,50 @@
+﻿namespace Digitally_Imported
+{
+    class Settings
+    {
+        /// <summary>
+        /// Актуальная ссылка на Temp-mail
+        /// </summary>
+        public static string TmURL { get; set; } = "https://temp-mail.ru/";
+
+        /// <summary>
+        /// Временный почтовый адрес
+        /// </summary>
+        public static string TmAddress { get; set; }
+
+        /// <summary>
+        /// Идентификатор письма с премиум-ссылкой
+        /// </summary>
+        public static string TmLetterID { get; set; }
+
+        /// <summary>
+        /// Актуальная ссылка на DI.fm
+        /// </summary>
+        public static string DiURL { get; set; } = "http://www.di.fm/";
+
+        /// <summary>
+        /// Токен для регистрации
+        /// </summary>
+        public static string DiToken { get; set; }
+
+        /// <summary>
+        /// Пароль от аккаунта
+        /// </summary>
+        public static string DiPass { get; set; }
+
+        /// <summary>
+        /// Премиум-ссылка из письма
+        /// </summary>
+        public static string DiPremiumURL { get; set; }
+
+        /// <summary>
+        /// Ключ слушателя
+        /// </summary>
+        public static string DiListenKey { get; set; }
+
+        /// <summary>
+        /// JSON-строка с информацией для плейлиста
+        /// </summary>
+        public static string DiPlaylistJS { get; set; }
+    }
+}
